7895c6e0d902c845d070dd3f196ab1e19179f226
rafal1behrendt@gmail.com <rafal1behrendt@gmail.com>
2019-10-27 20:11:16 +0100

    added declaration of constants per test, added function calls

--- homework.test.js
+++ homework.test.js
@@ -28,40 +28,68 @@ async function homework(a, b, send, next) {
 }
 
 describe('Homework unit tests', () => {
+
+
+    test('Should call send once with b as parameter when axios.get returns number bigger than b', () => {
         const a = 5
         const b = 10
         const send = jest.fn(x => x)
         const next = jest.fn(x => x)
 
-    test('Should call send once with b as parameter when axios.get returns number bigger than b or smaller than a', () => {
         jest.spyOn(axios, 'get').mockResolvedValue(b+4)
+
+        homework(a, b, send, next)
         expect(send.mock.calls.length).toBe(1)
         expect(send.mock.calls[0][0]).toEqual(b)
     })
 
-    test('Should call send once with b as parameter when axios.get returns number bigger than b or smaller than a', () => {
+    test('Should call send once with b as parameter when axios.get returns number smaller than a', () => {
+        const a = 5
+        const b = 10
+        const send = jest.fn(x => x)
+        const next = jest.fn(x => x)
+
+        
         jest.spyOn(axios, 'get').mockResolvedValue(a-3)
+        homework(a, b, send, next)
         expect(send.mock.calls.length).toBe(1)
         expect(send.mock.calls[0][0]).toEqual(b)
     })
 
     test('Should map get request error to custom error and throw it', () => {
+        const a = 5
+        const b = 10
+        const send = jest.fn(x => x)
+        const next = jest.fn(x => x)
+
         jest.spyOn(axios, 'get').mockRejectedValue('Getting error')
         return expect(homework(a, b, send, next)).rejects.toEqual({ msg: 'Error during fetching homework', error: 'Getting error' })
     })
 
     test('Should call next once with b as parameter when a is bigger or equal than result and b is bigger than result and posting is not succesful', () => {
+        const a = 5
+        const b = 10
+        const send = jest.fn(x => x)
+        const next = jest.fn(x => x)
+
         jest.spyOn(axios, 'post').mockRejectedValue("ERROR!")
         jest.spyOn(axios, 'get').mockResolvedValue(b)
+
+        homework(a, b, send, next)
+
         expect(send.mock.calls.length).toBe(1)
         expect(send.mock.calls[0][0]).toEqual(b)
     })
 
 
     test('Should call next once with post return statement as parameter when a is bigger or equal than result and b is smaller or equal than result and posting is succesful', () => {
+        const a = 5
+        const b = 10
+        const send = jest.fn(x => x)
+        const next = jest.fn(x => x)
+
         jest.spyOn(axios, 'get').mockResolvedValue(7)
         const rtrnStmnt = jest.spyOn(axios, 'post').mockResolvedValue(a+b)
-
         homework(a, b, send, next)
         
         expect(next.mock.calls.length).toBe(1)
@@ -69,6 +97,11 @@ describe('Homework unit tests', () => {
     })
 
     test('Should call next once with result as parameter when a is bigger or equal than result and b is smaller than result and posting is not succesful', () => {
+        const a = 5
+        const b = 10
+        const send = jest.fn(x => x)
+        const next = jest.fn(x => x)
+
         jest.spyOn(axios, 'get').mockResolvedValue(7)
         jest.spyOn(axios, 'post').mockResolvedValue(a+b)
 
@@ -79,12 +112,21 @@ describe('Homework unit tests', () => {
     })
 
     test('Should map post request error to custom error and throw it', () => {
+        const a = 5
+        const b = 10
+        const send = jest.fn(x => x)
+        const next = jest.fn(x => x)
+
         jest.spyOn(axios, 'get').mockResolvedValue(7)
         jest.spyOn(axios, 'post').mockRejectedValue('Posting error')
         return expect(homework(a, b, send, next)).rejects.toEqual({ msg: 'Error during posting homework result', error: 'Posting error' })
     })
 
     test('Should return a+b when posting homework results', () => {
+        const a = 5
+        const b = 10
+        const send = jest.fn(x => x)
+        const next = jest.fn(x => x)
 
         jest.spyOn(axios, 'get').mockResolvedValue(7)
         jest.spyOn(axios, 'post').mockResolvedValue(a+b)
